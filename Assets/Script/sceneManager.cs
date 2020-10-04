26405293f79f7e684c5d19e69bcdd9797a92601f
Rexfortiz <daydreamer9422@gmail.com>
2020-10-04 21:26:10 +0700

    Minggu 4 Tugas
    Thomas Farel 01918147226

---
+++ Assets/Script/sceneManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneManager : MonoBehaviour
+{
+    public string EnterScene;
+    public string EscapeScene;
+    public bool isEscapeForQuit = false;
+
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Return))
+        {
+            Debug.Log("Name Scene: " + EnterScene);
+            SceneManager.LoadScene(EnterScene);
+        }
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            if (isEscapeForQuit)
+            {
+                Application.Quit();
+            }
+            else
+            {
+                Debug.Log("Name Scene: " + EscapeScene);
+                SceneManager.LoadScene(EscapeScene);
+            }
+        }
+    }
+}
